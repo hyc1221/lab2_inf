507b082ce9e7d15fd7466014bc532e8fccd2a1a9
hycpower1221 <hycpower1221@gmail.com>
2019-10-04 22:56:32 +0300

    cc

--- lab2/lab2/Form1.Designer.cs
+++ lab2/lab2/Form1.Designer.cs
@@ -68,7 +68,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(174, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Расчитать";
+            this.button1.Text = "Расcчитать";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
