73e91b27659e08dced845d153417f46491083ef0
hycpower1221 <hycpower1221@gmail.com>
2019-10-04 19:30:42 +0300

    количество экспер. + изменение размерности всех массивов

--- lab2/lab2/Form1.Designer.cs
+++ lab2/lab2/Form1.Designer.cs
@@ -32,13 +32,18 @@
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
             this.richTextBox1.Location = new System.Drawing.Point(13, 13);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(181, 425);
+            this.richTextBox1.Size = new System.Drawing.Size(181, 364);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
@@ -54,33 +59,87 @@
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 4F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.richTextBox2.Location = new System.Drawing.Point(201, 13);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(848, 425);
+            this.richTextBox2.Size = new System.Drawing.Size(848, 364);
             this.richTextBox2.TabIndex = 2;
             this.richTextBox2.Text = "";
             this.richTextBox2.WordWrap = false;
             // 
             // richTextBox3
             // 
-            this.richTextBox3.Location = new System.Drawing.Point(13, 445);
+            this.richTextBox3.Location = new System.Drawing.Point(13, 383);
             this.richTextBox3.Name = "richTextBox3";
-            this.richTextBox3.Size = new System.Drawing.Size(181, 307);
+            this.richTextBox3.Size = new System.Drawing.Size(181, 369);
             this.richTextBox3.TabIndex = 3;
             this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(200, 383);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(849, 369);
+            this.richTextBox4.TabIndex = 4;
+            this.richTextBox4.Text = "";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(1096, 42);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(1096, 69);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDown2.TabIndex = 6;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1183, 764);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.richTextBox4);
             this.Controls.Add(this.richTextBox3);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -91,6 +150,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
     }
 }
 
--- lab2/lab2/Form1.cs
+++ lab2/lab2/Form1.cs
@@ -17,13 +17,15 @@ namespace lab2
             InitializeComponent();
         }
         static int N = 28;
-        static double[] px;
-        static double[,] pxy;
+        static double[,] px;
+        static double[,,] pxy;
         static int round = 5;
         static Random rand = new Random();
+        static int K = 0;
 
-        public void Output_pxy()
+        public void Output_pxy(int k)
         {
+            richTextBox2.Clear();
             double s = 0;
             richTextBox3.AppendText("p(x/y):\n");
             for (int i = 0; i < N; i++)
@@ -31,8 +33,8 @@ namespace lab2
                 s = 0;
                 for (int j = 0; j < N; j++)
                 {
-                    richTextBox2.AppendText(String.Format("{0:f17} ", pxy[i, j]));
-                    s += pxy[i, j];
+                    richTextBox2.AppendText(String.Format("{0:f17} ", pxy[k, i, j]));
+                    s += pxy[k, i, j];
                 }
                 richTextBox2.AppendText("\n");
                 richTextBox3.AppendText("s[" + (i + 1).ToString() + "] = " + s.ToString() + "\n");
@@ -40,33 +42,46 @@ namespace lab2
             richTextBox3.AppendText("\n");
             for (int i = 0; i < N; i++)
             {
-                richTextBox2.Find(String.Format("{0:f17} ", pxy[i, i]));
+                richTextBox2.Find(String.Format("{0:f17} ", pxy[k, i, i]));
                 richTextBox2.SelectionColor = Color.Red;
             }
         }
 
-
-        public void Calc_pxy()
+        public void Output_px(int k)
         {
-            pxy = new double[N, N];
-            
-            for (int h = 0; h < N; h++) pxy[h, h] = 0.7 + 0.3 * rand.NextDouble(); 
+            richTextBox3.Clear();
+            richTextBox1.Clear();
+            double s = 0;
             for (int i = 0; i < N; i++)
             {
-                double b = 1 - pxy[i, i];
-                for (int j = 0; j < N; j++)
+                richTextBox1.AppendText("px[" + (i + 1).ToString() + "] = " + Math.Round(px[k, i], round).ToString() + "\n");
+                s += px[k, i];
+            }
+            richTextBox3.AppendText("p(x): \n" + "s = " + s.ToString() + "\n\n");
+        }
+
+        public void Calc_pxy(int kv)
+        {
+            pxy = new double[kv, N, N];
+            for (int k = 0; k < K; k++)
+            {
+                for (int h = 0; h < N; h++) pxy[k, h, h] = 0.7 + 0.3 * rand.NextDouble();
+                for (int i = 0; i < N; i++)
                 {
-                    if (i != j)
+                    double b = 1 - pxy[k, i, i];
+                    for (int j = 0; j < N; j++)
                     {
-                        pxy[i, j] = b * rand.NextDouble();
-                        b -= pxy[i, j];
+                        if (i != j)
+                        {
+                            pxy[k, i, j] = b * rand.NextDouble();
+                            b -= pxy[k, i, j];
+                        }
                     }
                 }
             }
-   
         }
 
-        public void Calc_px()
+        public void Calc_px(int kv)
         {
             //  N = Convert.ToInt32(numericUpDown1.Value);
             // int K = Convert.ToInt32(numericUpDown2.Value);
@@ -77,52 +92,66 @@ namespace lab2
             // for (int k = 1; k <= K; k++)
             // {
             // richTextBox1.AppendText("Эксперимент: " + k.ToString() + "\n");
-            px = new double[N];
-            
-            double b = 0;
-            double s = 0;
-            int offset = 0;
-            int count = 0;
-            if (N % 10 > 0 || N == 10)
+            px = new double[kv, N];
+            for (int k = 0; k < K; k++)
             {
-                b = 1 / (Convert.ToDouble(N) / 2);
-                offset = N / (N / 2);
-            }
-            else
-            {
-                b = 1.0 / 10;
-                offset = N / 10;
-            }
-            while (s != 1 && count < N)
-            {
-                double bv = b;
-                double sv = 0;
-                for (int i = count; i < count + offset - 1; i++)
+                double b = 0;
+                double s = 0;
+                int offset = 0;
+                int count = 0;
+                if (N % 10 > 0 || N == 10)
                 {
-                    px[i] = bv * rand.NextDouble();
-                    bv -= px[i];
-                    sv += px[i];
-
+                    b = 1 / (Convert.ToDouble(N) / 2);
+                    offset = N / (N / 2);
                 }
-                px[count + offset - 1] = b - sv;
-                sv += px[count + offset - 1];
-                s += sv;
-                count += offset;
+                else
+                {
+                    b = 1.0 / 10;
+                    offset = N / 10;
+                }
+                while (s != 1 && count < N)
+                {
+                    double bv = b;
+                    double sv = 0;
+                    for (int i = count; i < count + offset - 1; i++)
+                    {
+                        px[k, i] = bv * rand.NextDouble();
+                        bv -= px[k, i];
+                        sv += px[k, i];
+
+                    }
+                    px[k, count + offset - 1] = b - sv;
+                    sv += px[k, count + offset - 1];
+                    s += sv;
+                    count += offset;
+                }
+             //   richTextBox1.AppendText("\n");
+                
+              
             }
-            richTextBox1.AppendText("\n");
-            for (int i = 0; i < N; i++)
-            {
-                richTextBox1.AppendText("px[" + (i + 1).ToString() + "] = " + Math.Round(px[i], round).ToString() + "\n");
-            }
-            richTextBox3.AppendText("p(x): \n" + "s = " + s.ToString() + "\n\n");
         }
 
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Calc_px();
-            Calc_pxy();
-            Output_pxy();
+            K = Convert.ToInt32(numericUpDown1.Value);
+            int k = Convert.ToInt32(numericUpDown2.Value) - 1;
+            Calc_px(K);
+            Calc_pxy(K);
+            Output_px(k);
+            Output_pxy(k);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown2.Maximum = numericUpDown1.Value;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            int k = Convert.ToInt32(numericUpDown2.Value) - 1;
+            Output_px(k);
+            Output_pxy(k);
         }
     }
 }
